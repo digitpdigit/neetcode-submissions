74771458f29fe873c26f294d5518f2aac13130db
MrSandman <67557217+digitpdigit@users.noreply.github.com>
2026-06-03 16:35:35 +0700

    Add: evaluate-reverse-polish-notation - submission-4

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operation_set = set(["+", "-", "/", "*"])
+        q = collections.deque()
+
+        # We will have LIFO queue, we put token there
+        # Until we found the operation
+        # then we popleft operation and do the thing
+        # Then we put it back on the queue
+        # tokens=["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+        # 10 * (6 / ((9 + 3) * -11)) + 17 + 5
+        
+        for token in tokens:
+            if not token in operation_set:
+                q.append(int(token))
+            else:
+                second = q.pop()
+                first = q.pop()
+
+                # At this point q should be zero
+                if token == "+":
+                    q.append(first + second)
+                if token == "-":
+                    q.append(first - second)
+                if token == "*":
+                    q.append(first * second)
+                if token == "/":
+                    q.append(int(first / second))
+
+        return q[0]
+        
